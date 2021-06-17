882b88300d93663a08ebeffa1c70200d6e495197
wll <1909302406@qq.com>
2021-06-17 17:27:55 +0800

    ggg

--- Model_BiGRU/config.py
+++ Model_BiGRU/config.py
@@ -4,7 +4,7 @@ class Config(object):
     embed_size = 200#w2v维度
     pos_embed_size =20#pos维度
     hidden_layers = 2
-    hidden_size = 200
+    hidden_size = 300
     bidirectional = True
     output_size = 5
     max_epochs = 30
